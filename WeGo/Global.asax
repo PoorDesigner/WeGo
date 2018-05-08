2940f4d7783b8d8e7894d8daeb7671860a626557
Vivek <vivekh.meka@gmail.com>
2018-05-08 22:18:22 +0530

    Add project files.

---
+++ WeGo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WeGo.MvcApplication" Language="C#" %>
---
+++ WeGo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WeGo.Startup))]
+namespace WeGo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
